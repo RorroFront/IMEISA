094730351fd362c27ddd827d07bcde8806dfe785
Adrian Front <rathaloscore@gmail.com>
2025-05-26 22:43:46 -0600

    mobile update

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { OurservicesComponent } from "./ourservices/ourservices.component";
+import { UsComponent } from "./us/us.component";
+import { ClientsComponent } from "./clients/clients.component";
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterModule,
     RouterOutlet,
-    OurservicesComponent
+    OurservicesComponent,
+    UsComponent,
+    ClientsComponent
 ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
---
+++ src/app/layout/us/us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-us',
+  imports: [],
+  templateUrl: './us.component.html',
+  styleUrl: './us.component.css'
+})
+export class UsComponent {
+
+}
